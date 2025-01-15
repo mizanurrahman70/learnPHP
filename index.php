67ee1a54170f0ad650b249422dc4b00e4bedc93c
Md Mizanur Rahman <mizanurrahmanofficial70@gmail.com>
2025-01-15 19:25:19 +0600

    form explore

--- index.php
+++ index.php
@@ -7,13 +7,28 @@
 </head>
 <body>
 <?php
-$a = 1;
+// $a = 1;
 
-while ($a <= 10) {
-    $a ++ ;
-    echo('hello world' . '<br/>');
-}
+// while ($a <= 10) {
+//     $a ++ ;
+//     echo('hello world' . '<br/>');
+// }
+echo '<pre>';
+var_dump($_POST);
+echo '</pre>'
 ?>
+<form action ='<?php echo $_SERVER['PHP_SELF'] ?>' method ='POST'>
+    <div>
+        <input type ='text' name='first_name' placeholder='First Name'/>
+</div>
+    <div>
+        <input type ='text' name='last_name' placeholder='Last Name'/>
+</div>
+    <div>
+        <input type ='text' name='email_address' placeholder='email Address'/>
+</div>
+<button type='submit'>Submit </button>
+</form>
 
 </body>
 </html>
